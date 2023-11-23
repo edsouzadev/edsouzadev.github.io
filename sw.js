efb372b819ce1b2a0afc1b705cdd9100811509c3
edsouzadev <edsouza.dev@gmail.com>
2023-11-23 10:29:03 -0300

    atualizacao 01

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700501758';
+const cacheName = 'chirpy-1700746053';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
